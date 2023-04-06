803b667256de44683e494b68fa9ed2f44bb366dc
damoncho <99659620+damoncho@users.noreply.github.com>
2023-04-06 19:54:06 +0900

    Add files via upload

---
+++ editor/script.js
@@ -0,0 +1,42 @@
+const md = window.markdownit({
+  html: true,
+  breaks: true,
+  linkify: true,
+  typographer: true
+});
+
+const form = document.querySelector('form');
+const markdownInput = document.getElementById('markdown-input');
+const htmlOutput = document.getElementById('html-output');
+const textarea = document.querySelector('textarea');
+
+textarea.addEventListener('keydown', event => {
+  if (event.key === '\\') {
+    event.preventDefault();
+    const start = textarea.selectionStart;
+    const end = textarea.selectionEnd;
+    const value = textarea.value;
+    textarea.value = value.substring(0, start) + '\n' + value.substring(end);
+    textarea.selectionStart = textarea.selectionEnd = start + 1;
+  }
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const html = md.render(markdownInput.value);
+  htmlOutput.innerHTML = html;
+});
+
+const downloadButton = document.getElementById('download-html');
+downloadButton.addEventListener('click', async () => {
+  const html = htmlOutput.innerHTML;
+  const blob = new Blob([html], { type: 'text/html' });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = 'preview.html';
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+  URL.revokeObjectURL(url);
+});
