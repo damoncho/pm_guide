abc10ac25a6cafdaddf59c6f8295cff3ef13db19
damoncho <99659620+damoncho@users.noreply.github.com>
2023-04-08 19:36:20 +0900

    Create scripts.js

---
+++ slider/scripts.js
@@ -0,0 +1,31 @@
+const container = document.querySelector('.swiper-container');
+const slides = document.querySelectorAll('.swiper-slide');
+const prevButton = document.querySelector('.swiper-button-prev');
+const nextButton = document.querySelector('.swiper-button-next');
+
+let currentIndex = 0;
+
+prevButton.addEventListener('click', () => {
+  if (currentIndex === 0) {
+    currentIndex = slides.length - 1;
+  } else {
+    currentIndex--;
+  }
+  updateSlide();
+});
+
+nextButton.addEventListener('click', () => {
+  if (currentIndex === slides.length - 1) {
+    currentIndex = 0;
+  } else {
+    currentIndex++;
+  }
+  updateSlide();
+});
+
+function updateSlide() {
+  slides.forEach((slide, index) => {
+    slide.style.transform = `translateX(${(index - currentIndex) * 100}%)`;
+    slide.style.zIndex = index === currentIndex ? 1 : 0;
+  });
+}
